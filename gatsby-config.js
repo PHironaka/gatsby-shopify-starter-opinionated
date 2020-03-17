103dd2f9e0716ba49f3f97ac39ef33b7d89b1288
Peter Hironaka <peterhironaka@Peters-MacBook-Pro.local>
2020-03-17 15:09:30 -0700

    update the node

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,17 +42,9 @@ module.exports = {
       },
     },
 
-    // {
-    //   resolve: `gatsby-plugin-intercom`,
-    //   options: {
-    //     appId: 'YOUR_INTERCOM_APP_ID',
-    //   },
-    // },
-
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
-        // Add any options here
         displayName: false
       },
     },
@@ -61,23 +53,8 @@ module.exports = {
     {
       resolve: `gatsby-source-shopify`,
       options: {
-        // The domain name of your Shopify shop. This is required.
-        // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
-        // 'gatsby-source-shopify-test-shop.myshopify.com'.
         shopName: process.env.SHOP_NAME,
-
-        // An API access token to your Shopify shop. This is required.
-        // You can generate an access token in the "Manage private apps" section
-        // of your shop's Apps settings. In the Storefront API section, be sure
-        // to select "Allow this app to access your storefront data using the
-        // Storefront API".
-        // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
         accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
-
-        // Set verbose to true to display a verbose output on `npm run develop`
-        // or `npm run build`. This prints which nodes are being fetched and how
-        // much time was required to fetch and process the data.
-        // Defaults to true.
         verbose: true,
       },
     },
@@ -87,16 +64,7 @@ module.exports = {
         '~': path.join(__dirname, 'src/'),
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: "UA-134421805-1",
-    //     anonymize: true,
-    //     respectDNT: true,
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
+
     'gatsby-plugin-offline',
   ],
 }
